b0e9c616eace255c138a03903ddfa01cf8bf7269
zsavery <zsavery998@gmail.com>
2025-08-28 05:36:05 +0000

    add output examples
    a simple hello world spript followed by a my name is programm

---
+++ output/1_echo.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+# Author: zsavery
+# Purpose: Denitrate displaying text in terminal
+
+echo "Hello World!"
---
+++ output/2_variable.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+# Author: zsavery
+# Purpose: Denitrate how to use variables in shell scripting
+
+echo "Hello World!"
+
+# Assigning a value to a variable
+name="zsavery" # no spaces around the equal sign
+
+# Accessing the value of a variable
+echo "My name is $name" # using $ to access variable value
